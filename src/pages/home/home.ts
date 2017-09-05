bb7a9cf0901117564eb68f43d25af7db02bbd19e
Joe Maguire <joe.j.maguire@gmail.com>
2017-09-04 22:23:06 -0500

    Implement a slides control that dynamically generates slides based on a collection of recipes
    Current limitations:
     - only one recipe of "real" data
     - same image displays for all recipes
     - the max height of all slides is the same as the longest which leads to unwanted scrolling ability
     - I'm not really making use of typescript at all atm
     - recipes should be pulled in via a service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+
+  recipes: object[];
 
   constructor(public navCtrl: NavController) {
+    this.recipes = [
+      {
+        name: "Basic Fruit Salad",
+        ingredients: [
+          "1 apple",
+          "1 banana",
+          "1 orange",
+          "1 cup grapes"
+        ],
+        instructions: [
+          "Cut the apple into cubes.",
+          "Cut the banana into slices.",
+          "Peel the orange, and divide into segments.",
+          "Combine all ingredients in a bowl.",
+          "Mix to combine.",
+          1,1,1,3,4,1,1,1,1,1
+        ],
+        cookTime: "N/A",
+        prepTime: "20 min",
+        yield: "5 servings"
+      },
+      {
+        name: 2,
+        ingredients: [1,2,3,4,5],
+        instructions: [1,2,3,4,5]
+      },
+      {
+        name: 3,
+        ingredients: [1,2,3,4,5],
+        instructions: [1,2,3,4,5]
+      }
+    ]
+  }
 
+  onSlideChanged() {
+    this.slides.setElementStyle('height', 'auto')
   }
 
 }
